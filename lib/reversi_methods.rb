cfe9aed2b8058d77f68291e009406c2e08914f0f
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-09-01 11:44:07 +0900

    Revert "cell == BLANK_CELLを削除してテストが落ちることの確認をした"
    This reverts commit 61e3e83c693a2b6758f420a3228d1bca7fb4abe8.

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -82,7 +82,7 @@ module ReversiMethods
     board.each_with_index do |cols, row|
       cols.each_with_index do |cell, col|
         position = Position.new(row, col)
-        return true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
+        return true if cell == BLANK_CELL && put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
       end
     end
     false
